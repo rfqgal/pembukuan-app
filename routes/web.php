30a7be952693831c8c49dc30578e05bd0922741f
Rifky <rfqgal@gmail.com>
2023-11-24 20:40:36 +0700

    feat(route): create dashboard & income/outcome

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IncomeController;
+use App\Http\Controllers\OutcomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -17,22 +20,23 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('dashboard');
 });
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('dashboard')->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('/api/income', [DashboardController::class, 'incomeApi'])->name('dashboard.api.income');
+        Route::get('/api/outcome', [DashboardController::class, 'outcomeApi'])->name('dashboard.api.outcome');
+    });
+
+    Route::resource('income', IncomeController::class);
+    
+    Route::resource('outcome', OutcomeController::class);
 });
 
 require __DIR__.'/auth.php';
