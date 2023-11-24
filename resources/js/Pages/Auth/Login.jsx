e396e0ec9682187f58562f733b9a950aca3d3558
Rifky <rfqgal@gmail.com>
2023-11-24 20:39:32 +0700

    fix(pages): simplify login and edit profile

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,13 +1,12 @@
 import { useEffect } from 'react';
-import { Head, Link, useForm } from '@inertiajs/react';
-import Checkbox from '@/Components/Checkbox';
+import { Head, useForm } from '@inertiajs/react';
 import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
 
-export default function Login({ status, canResetPassword }) {
+export default function Login({ status }) {
   const {
     data, setData, post, processing, errors, reset,
   } = useForm({
@@ -68,30 +67,9 @@ export default function Login({ status, canResetPassword }) {
           <InputError message={errors.password} className="mt-2" />
         </div>
 
-        <div className="block mt-4">
-          <label className="flex items-center">
-            <Checkbox
-              id="remember"
-              name="remember"
-              checked={data.remember}
-              onChange={(e) => setData('remember', e.target.checked)}
-            />
-            <span className="ms-2 text-sm text-gray-600">Remember me</span>
-          </label>
-        </div>
-
-        <div className="flex items-center justify-end mt-4">
-          {canResetPassword && (
-            <Link
-              href={route('password.request')}
-              className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Forgot your password?
-            </Link>
-          )}
-
+        <div className="flex items-center justify-end mt-6">
           <PrimaryButton type="submit" className="ms-4" disabled={processing}>
-            Log in
+            Login
           </PrimaryButton>
         </div>
       </form>
