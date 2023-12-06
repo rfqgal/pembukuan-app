66461dc96fff87b6d9262df39d58ac51c98c8b36
Rifky <rfqgal@gmail.com>
2023-12-06 20:59:59 +0700

    fix(layout): remove min height screen

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -160,7 +160,7 @@ export default function AuthenticatedLayout({ user, children, hideFilter }) {
           </div>
         </Header>
         {children
-          ? <main className="min-h-screen p-6 space-y-4">{children}</main>
+          ? <main className="p-6 space-y-4">{children}</main>
           : (
             <main className="min-h-screen p-6">
               <Content
